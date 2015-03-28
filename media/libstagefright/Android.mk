019534fdaffdeca7c8eb3c5200795b6c92904dd0
martinusbe <martinusbe@gmail.com>
2015-03-28 16:20:59 +0100

    fix build
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -84,8 +84,7 @@ LOCAL_SRC_FILES:=                         \
         ExtendedExtractor.cpp             \
         ExtendedUtils.cpp                 \
         ExtendedStats.cpp                 \
-        APE.cpp                           \
-        FFMPEGSoftCodec.cpp               \
+        APE.cpp                           
 
 LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/av/include/media/ \
